cfabf9934f976683e15dfbd9b58f9037ac0efbf3
Bob <danyalmeidaconsultora@gmail.com>
2024-07-09 22:11:29 -0300

    ci-cd

--- cypress/e2e/busca.cy.js
+++ cypress/e2e/busca.cy.js
@@ -5,27 +5,31 @@ describe('US-001-Funcionalidade: Busca de filmes', () => {
         cy.visit('/')
     });
 
+    afterEach(() => {
+        cy.screenshot()
+    });
+
     it('Deve buscar filmes com sucesso', () => {
         cy.get('#search-input').type('Matrix')
         cy.get('#search-button').click()
         cy.get('#results-section').should('contain', 'Matrix')
 
-});
+    });
 
-it('Deve buscar com sucesso de uma lista', () => {
-    cy.fixture('filmes').then((filmes) => {
-        cy.get('#search-input').type(filmes[1].titulo)
-        cy.get('#search-button').click()
-        cy.get('#results-section').should('contain', filmes[0].titulo)
-    })
-});
+    it('Deve buscar com sucesso de uma lista', () => {
+        cy.fixture('filmes').then((filmes) => {
+            cy.get('#search-input').type(filmes[0].titulo)
+            cy.get('#search-button').click()
+            cy.get('#results-section').should('contain', filmes[0].titulo)
+        })
+    });
 
-it.only('Deve buscar com sucesso de uma lista inteira', () => {
-    cy.fixture('filmes').each((filmes) => {
-        cy.get('#search-input').clear().type(filmes.titulo)
-        cy.get('#search-button').click({ force: true })
-        cy.get('#results-section').should('contain', filmes.titulo)
-    })
-});
+    it('Deve buscar com sucesso de uma lista inteira', () => {
+        cy.fixture('filmes').each((filmes) => {
+            cy.get('#search-input').clear().type(filmes.titulo)
+            cy.get('#search-button').click({ force: true })
+            cy.get('#results-section').should('contain', filmes.titulo)
+        })
+    });
 
 });
--- cypress/e2e/cadastro.cy.js
+++ cypress/e2e/cadastro.cy.js
@@ -4,39 +4,43 @@ describe('US-012-Funcionalidade: Cadastro de membro', () => {
   beforeEach(() => {
     cy.visit('/')
   });
-  
+
+  afterEach(() => {
+    cy.screenshot()
+  });
+
   it('Deve fazer o cadastro de campos obrigatórios', () => {
     var email = `daniela${Date.now()}@teste.com`
-    cy.preencherCadastro('daniela', 'Almeida', email, '71958476214', 'Teste!3030' )
+    cy.preencherCadastro('daniela', 'Almeida', email, '71958476214', 'Teste!3030')
     cy.get('#signup-response').should('contain', 'Cadastro realizado com sucesso!')
-  })
+  });
 
   it('Deve validar mensagem de erro com campo nome inválido', () => {
     cy.preencherCadastro('daniela50', 'Almeida', 'daniela@teste.com', '71958476214', 'Teste!3030')
-    cy.get('#signup-response'.should('contain', 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços'))
+    cy.get('#signup-response').should('contain', 'Nome deve conter apenas caracteres alfabéticos, acentuados e espaços')
+  });
+
+  it('Deve validar mensagem de erro com campo sobrenome inválido', () => {
+    cy.preencherCadastro('daniela', 'Almeida258', 'daniela@teste.com', '71958476214', 'Teste!3030')
+    cy.get('#signup-response').should('contain', 'Sobrenome deve conter apenas caracteres alfabéticos, acentuados e espaços')
   });
 
   it('Deve validar mensagem de erro com campo email inválido', () => {
     cy.preencherCadastro('daniela', 'Almeida', 'daniela.teste.com', '71958476214', 'Teste!3030')
-    cy.get('#signup-response'.should('contain', 'E-mail deve ser um email válido'))
-   
+    cy.get('#signup-response').should('contain', 'E-mail deve ser um email válido')
+
   });
 
-  it('Deve validar mensagem de erro com campo nome inválido', () => {
+  it('Deve validar mensagem de erro com campo telefone inválido', () => {
     cy.preencherCadastro('daniela', 'Almeida', 'daniela@teste.com', 'adseee', 'teste!3030')
-    cy.get('#signup-response'.should('contain', 'Telefone deve conter apenas números'))
+    cy.get('#signup-response').should('contain', 'Telefone deve conter apenas números')
   });
 
   it('Deve validar mensagem de erro com campo senha', () => {
     cy.preencherCadastro('daniela', 'Almeida', 'daniela@teste.com', '71958476214', 'teste!3030')
-    cy.get('#signup-response'.should('contain', 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula, um número e um caractere especial (!@#$&*'))
-   
+    cy.get('#signup-response').should('contain', 'Senha deve ter pelo menos 8 caracteres, incluir uma letra maiúscula, um número e um caractere especial (!@#$&*')
+
   });
 
 
-
-
-
-
-
-})
+});
